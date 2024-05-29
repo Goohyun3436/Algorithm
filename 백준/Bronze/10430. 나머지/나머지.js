87634887c871209ad142b91a6aefe9d874e02e8d
Goo Hyun <94745558+Goohyun3436@users.noreply.github.com>
2024-05-29 17:26:39 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const [A, B, C] = input.map(Number);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
