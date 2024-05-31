e1d07fa8d2814491eea43687efe9eac7672cabbf
Goo Hyun <94745558+Goohyun3436@users.noreply.github.com>
2024-05-31 14:20:25 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 112 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -1,2 +1,2 @@
 let input = require('fs').readFileSync(0, 'utf-8').trim();
-console.log(input == [...input.split('')].reverse().join('') ? 1 : 0);
+console.log(input === [...input.split('')].reverse().join('') ? 1 : 0);
