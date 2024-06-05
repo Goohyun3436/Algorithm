7834c4ed43eb5b4f6df6e00f868e673a129fe7fc
Goo Hyun <94745558+Goohyun3436@users.noreply.github.com>
2024-06-05 22:03:09 +0900

    [Silver V] Title: 너의 평점은, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,27 @@
+let input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+
+const toScore = {
+  'A+': 4.5,
+  'A0': 4.0,
+  'B+': 3.5,
+  'B0': 3.0,
+  'C+': 2.5,
+  'C0': 2.0,
+  'D+': 1.5,
+  'D0': 1.0,
+  'F': 0.0,
+};
+
+let hours = 0;
+let scores = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const [subject, hour, grade] = input[i].split(' ');
+
+  if (grade !== 'P') {
+    hours += Number(hour);
+    scores += Number(hour) * toScore[grade];
+  }
+}
+
+console.log((scores / hours).toFixed(6));
