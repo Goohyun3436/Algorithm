d706e0e9d32c93bd5047c8dc28c3bc11c278c076
Goo Hyun <94745558+Goohyun3436@users.noreply.github.com>
2024-06-09 23:27:00 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 9688 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync(0, 'utf-8').trim().split('\n').map(el => el.split(' ').map(Number));
+const N = parseInt(input.shift());
+
+let whiteArea = Array.from(Array(100), () => new Array(100).fill(0));
+for (let [x, y] of input) {
+  for (let m = 0; m < 10; m++) {
+    for (let n = 0; n < 10; n++) {
+      whiteArea[y + m][x + n] = 1;
+    }
+  }
+}
+
+let blackArea = 0;
+whiteArea.map(el => (blackArea += el.filter(color => color === 1).length));
+console.log(blackArea);
