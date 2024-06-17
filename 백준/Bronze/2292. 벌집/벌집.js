a8f19d67837503d09f5851b0a30baddbb1b50269
Goo Hyun <rngus3436@g.kmou.ac.kr>
2024-06-17 19:51:56 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.js
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -1,10 +1,8 @@
-let N = parseInt(require('fs').readFileSync(0, 'utf-8').trim());
+let N = parseInt(require('fs').readFileSync(0, 'utf-8'));
 let count = 1;
 let maxOfBorder = 1;
 
-while (true) {
-  if (N <= maxOfBorder) break;
-
+while (maxOfBorder < N) {
   maxOfBorder += 6 * count;
   count++;
 }
