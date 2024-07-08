e6f77011b108192882d32975026298cab55ea5a1
Goo Hyun <rngus3436@g.kmou.ac.kr>
2024-07-08 19:52:41 +0900

    [Bronze III] Title: 오븐 시계, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.swift
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.swift
@@ -2,10 +2,7 @@ var now: [Int] = readLine()!.split(separator:" ").map{ Int($0)! }
 let takeTime: Int = Int(readLine()!)!
 
 now[1] += takeTime
+now[0] = (now[0] + now[1] / 60) % 24
+now[1] %= 60
 
-if (now[1] > 59) {
-    now[0] += Int(now[1] / 60)
-    now[1] %= 60    
-    if (now[0] > 23) { now[0] -= 24 }
-}
 print("\(now[0]) \(now[1])")
