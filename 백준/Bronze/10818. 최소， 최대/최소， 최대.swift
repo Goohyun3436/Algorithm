8ae6efaedf19608d9319e22e2770f332aa001587
H.GOO <rngus3436@g.kmou.ac.kr>
2024-07-12 17:15:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 408 ms, Memory: 133696 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.swift
@@ -0,0 +1,16 @@
+let n = Int(readLine()!)!
+let input: [Int] = readLine()!.split(separator: " ").map{ Int($0)! }
+
+var max: Int = -1000000
+var min: Int = +1000000
+
+for num in input {
+  if num > max {
+    max = num
+  }
+
+  if num < min {
+    min = num
+  }
+}
+print("\(min) \(max)")
