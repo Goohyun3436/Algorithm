a5df9c70c188d875de1ad6830e6564ccfcaa1db4
Goo Hyun <rngus3436@g.kmou.ac.kr>
2024-07-12 13:27:32 +0900

    [Bronze V] Title: A+B - 8, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.swift
@@ -0,0 +1,7 @@
+var res: [String] = []
+
+for i in 1 ... Int(readLine()!)! {
+  let input: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+  res.append("Case #\(i): \(input[0]) + \(input[1]) = \(input[0] + input[1])")
+}
+print(res.joined(separator: "\n"))
