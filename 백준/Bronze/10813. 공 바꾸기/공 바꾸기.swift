9b559046a30d3e91514ba44a7a1452316801a082
H.GOO <rngus3436@g.kmou.ac.kr>
2024-07-14 16:46:17 +0900

    [Bronze II] Title: 공 바꾸기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
@@ -4,11 +4,6 @@ var basket: [String] = Array(1 ... n).map { String($0) }
 
 for _ in 0 ..< m {
   let input: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
-  
-  let iBall: String = basket[input[0] - 1]
-  let jBall: String = basket[input[1] - 1]
-    
-  basket[input[0] - 1] = jBall
-  basket[input[1] - 1] = iBall
+  basket.swapAt(input[0] - 1, input[1] - 1)
 }
 print(basket.joined(separator: " "))
