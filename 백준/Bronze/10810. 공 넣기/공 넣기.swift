a2dc6f4bb5652882c3be25d18964cd70cf94e095
H.GOO <rngus3436@g.kmou.ac.kr>
2024-07-14 16:25:33 +0900

    [Bronze III] Title: 공 넣기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.swift
+++ 백준/Bronze/10810. 공 넣기/공 넣기.swift
@@ -1,5 +1,5 @@
 let cond: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
-let n: Int = cond[0], m: Int = cond[1]
+let (n, m): (Int, Int) = (cond[0], cond[1])
 var basket: [String] = Array(repeating: "0", count: n)
 
 for _ in 0 ..< m {
