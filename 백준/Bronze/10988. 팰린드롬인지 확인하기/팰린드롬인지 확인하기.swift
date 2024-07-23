5cc06add3fd8f68597abb79a7a48b3df250892e3
H.GOO <rngus3436@g.kmou.ac.kr>
2024-07-23 23:51:25 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
@@ -1,14 +1,2 @@
 let input: String = readLine()!
-print(findPalindrome(str: input) ? 1 : 0)
-
-func findPalindrome(str: String) -> Bool {
-    for i in 0 ..< str.count / 2 {
-        let left: Character = str[str.index(str.startIndex, offsetBy: i)]
-        let right: Character = str[str.index(str.startIndex, offsetBy: str.count - i - 1)]
-
-        if left != right {
-            return false
-        }
-    }
-    return true
-}
+print(input == String(input.reversed()) ? 1 : 0)
