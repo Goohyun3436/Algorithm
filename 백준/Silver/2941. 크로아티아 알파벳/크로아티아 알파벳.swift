97947962c056745c6bc54b0e7aaf2d73ea55dc8a
Goo <rngus3436@g.kmou.ac.kr>
2025-03-20 09:23:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 12 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var input = readLine()!
+let croatiaAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+croatiaAlphabet.forEach {
+    input = input.replacingOccurrences(of: $0, with: "@")
+}
+
+print(input.count)
+
+//let regex = /c=|c-|dz=|d-|lj|nj|s=|z=/
+//let count = input.replacing(regex, with: "@").count
+//print(count)
