a8c515bd09db1095b70ac1e56522b8f10a6b8fc2
Goo <rngus3436@g.kmou.ac.kr>
2025-03-29 17:33:47 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 12 ms, Memory: 69280 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.swift
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.swift
@@ -1,23 +1,22 @@
 let size = readLine()!.split(separator: " ")
-let M = Int(size[0])!
-let N = Int(size[1])!
+let (N, M) = (Int(size[0])!, Int(size[1])!)
 var answer = ""
 
-let row = Array(repeating: 0, count: N)
-var matrix = Array(repeating: row, count: M)
+let row = Array(repeating: 0, count: M)
+var matrix = Array(repeating: row, count: N)
 
-for j in 0..<M {
+for j in 0..<N {
     let row = readLine()!.split(separator: " ")
     
-    for i in 0..<N {
+    for i in 0..<M {
         matrix[j][i] += Int(row[i])!
     }
 }
 
-for j in 0..<M {
+for j in 0..<N {
     let row = readLine()!.split(separator: " ")
     
-    for i in 0..<N {
+    for i in 0..<M {
         answer += "\(matrix[j][i] + Int(row[i])!) "
     }
     
