f42869f6bed58f722b91df5fa2683e030bb8e5d9
Goo <rngus3436@g.kmou.ac.kr>
2025-04-03 15:00:16 +0900

    [Silver IV] Title: 카드2, Time: 20 ms, Memory: 80824 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.swift
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -1,45 +1,12 @@
-var queue = QueueWithArrayPointer<Int>()
+let n = Int(readLine()!)!
+var queue = Array(1...n)
+var front = 0
 
-for i in 1...Int(readLine()!)! {
-    queue.push(with: i)
+while queue.count - front > 1 {
+    front += 1
+    let top = queue[front]
+    front += 1
+    queue.append(top)
 }
 
-while queue.count > 1 {
-    queue.pop()
-    let top = queue.pop()!
-    queue.push(with: top)
-}
-
-print(queue.top()!)
-
-
-struct QueueWithArrayPointer<T> {
-    private var elements: [T] = []
-    private var front = 0
-    
-    var isEmpty: Bool {
-        elements.count - front < 1
-    }
-    
-    var count: Int {
-        elements.count - front
-    }
-    
-    mutating func push(with element: T) {
-        elements.append(element)
-    }
-    
-    @discardableResult
-    mutating func pop() -> T? {
-        if isEmpty {
-            return nil
-        } else {
-            defer { front += 1 }  //마지막에 실행
-            return elements[front]
-        }
-    }
-    
-    func top() -> T? {
-        return isEmpty ? nil : elements[front]
-    }
-}
+print(queue[front])
