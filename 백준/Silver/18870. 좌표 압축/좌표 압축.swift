b981d41291d264d4d581128ad862ea77d5257700
Goo <rngus3436@g.kmou.ac.kr>
2025-05-31 14:52:40 +0900

    [Silver II] Title: 좌표 압축, Time: 1208 ms, Memory: 135452 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.swift
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.swift
@@ -1,6 +1,7 @@
-let _ = readLine()
+let fileIO = FileIO()
+let n = fileIO.readInt()
 var dict: [Int: Int] = [:]
-let list = readLine()!.split(separator: " ").map { Int($0)! }
+let list = fileIO.readInts(count: n)
 let sorted = Set(list).sorted()
 
 for (idx, num) in sorted.enumerated() {
@@ -9,4 +10,43 @@ for (idx, num) in sorted.enumerated() {
 
 for num in list {
     print(dict[num]!, terminator: " ")
+}
+
+//MARK: - HelperAdd commentMore actions
+import Foundation
+
+final class FileIO {
+    private var buffer: [UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile()) + [UInt8(0)]
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1 : -1)
+    }
+
+    @inline(__always) func readInts(count: Int) -> [Int] {
+        return (0 ..< count).map { _ in readInt() }
+    }
 }
