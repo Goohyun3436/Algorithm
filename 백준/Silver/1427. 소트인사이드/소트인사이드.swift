12393af95429c4475828f93af50e97357a0b8e94
Goo <rngus3436@g.kmou.ac.kr>
2025-05-31 12:46:27 +0900

    [Silver V] Title: 소트인사이드, Time: 4 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.swift
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.swift
@@ -1,13 +1,2 @@
-let n = readLine()!
-var count = Array(repeating: 0, count: 10)
-var answer = ""
-
-for ch in n {
-    count[Int(String(ch))!] += 1
-}
-
-for num in stride(from: 9, through: 0, by: -1) {
-    answer += String(repeating: "\(num)", count: count[num])
-}
-
-print(answer)
+let n = readLine()!.sorted(by: >)
+print(String(n))
