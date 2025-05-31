06b2fbcf3db460b6f8c16e13a25f7c3554b3ad50
Goo <rngus3436@g.kmou.ac.kr>
2025-05-31 14:21:34 +0900

    [Silver V] Title: 나이순 정렬, Time: 192 ms, Memory: 74972 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
@@ -1,5 +1,4 @@
 struct User {
-    var id: Int = 0
     var name: String = ""
     var age: Int = 0
 }
@@ -9,12 +8,10 @@ var list: [User] = Array(repeating: User(), count: n)
 
 for i in 0..<n {
     let user = readLine()!.split(separator: " ")
-    list[i] = User(id: i, name: String(user[1]), age: Int(user[0])!)
+    list[i] = User(name: String(user[1]), age: Int(user[0])!)
 }
 
-list = list.sorted {
-    $0.age == $1.age ? $0.id < $1.id : $0.age < $1.age
-}
+list = list.sorted { $0.age < $1.age }
 
 for el in list {
     print(el.age, el.name)
